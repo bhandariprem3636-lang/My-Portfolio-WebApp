e4381b7830834e67614f69a7ed89a3c73f9826ea
bhandariprem3636-lang <bhandariprem3636@gmail.com>
2026-07-14 00:39:14 +0530

    experience page complete

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,8 @@
 import { PiCertificateDuotone, PiSuitcaseSimpleDuotone } from "react-icons/pi";
+import { SiGoogleclassroom } from "react-icons/si";
 import Disha from "/src/assets/dishaLogo.png";
 import infoLabz from "/src/assets/infolabz.png";
+import Regex from "/src/assets/regex.png";
 
 const Experience = () => {
   return (
@@ -89,17 +91,70 @@ const Experience = () => {
           </div>
         </div>
 
-        <div className="ml-10 mb-8 h-150 w-120 shadow-2xl/60 shadow-amber-200 hover:shadow-[0_0_25px_rgba(6,182,212,0.4),0_0_10px_rgba(6,182,212,0.6)] transform transition-all duration-500 ease-[cubic-bezier(0.34,1.56,0.64,1)]
-         hover:scale-[1.03] rounded-2xl">
-            
-             <div className="flex m-4 gap-2">
+        <div
+          className="ml-10 mb-8 h-150 w-120 shadow-2xl/60 hover:shadow-[0_0_25px_rgba(6,182,212,0.4),0_0_10px_rgba(6,182,212,0.6)] transform transition-all duration-500 ease-[cubic-bezier(0.34,1.56,0.64,1)]
+         hover:scale-[1.03] rounded-2xl"
+        >
+          <div className="flex m-4 gap-2">
             <div className="p-2 bg-cyan-300 rounded-full ">
+              <SiGoogleclassroom />
+            </div>
+
+            <p className="font-bold text-white m-0.5">
+              Training & Certifications !
+            </p>
+          </div>
+          <div className="w-full border border-gray-400"></div>
+
+          <div className="flex m-4 gap-3">
+            <div className="mt-1">
+              <img
+                src={Regex}
+                alt="infolabz-pic"
+                className="h-8  rounded-full shrink-0"
+              />
+            </div>
+
+            <div>
+              <p className="font-bold  text-blue-600 text-2xl">
+                June 2026 - Jul 2026 - Ahmedabad
+              </p>
+              <p className="font-bold text-sky-500 mt-1">Regex Software</p>
+              <p className="text-white text-xs font-bold mt-1">
+                Mordern React BootCamp
+              </p>
+              <p className="font-sans mt-2 text-gray-300 w-100">
+                I recently completed an intensive 30-day Modern React Bootcamp.
+                The program started with the history and core concepts of
+                JavaScript, followed by HTML and modern styling with Tailwind
+                CSS. We then transitioned into React, focusing on its essential
+                concepts. By building several mini-projects and completing
+                hands-on assignments, I have significantly strengthened my
+                practical understanding of both JS and React.
+              </p>
+            </div>
+          </div>
+          
+          <div className="flex m-4 gap-2">
+            <div className="p-2 bg-gray-300 rounded-full ">
               <PiCertificateDuotone />
             </div>
 
-            <p className="font-bold text-white m-0.5">Training & Certification !</p>
+            <p className="font-bold text-white m-0.5">Certification !</p>
           </div>
-          <div className="w-full border border-gray-400"></div>
+
+          <div className="m-4">
+
+            <ul className="ml-10 list-disc text-gray-300 font-bold">
+              <li>Mordern React Bootcamp (Regex Software)</li>
+              <li>Blockchain Full Stack Developer (Skill India)</li>
+              <li>Unlocking AI Everyone (Skill India)</li>
+              <li>Jr. Data Analyst (Infolabz Pvt. Ltd.)</li>
+              <li>Full-Stack Development 101 (Simplilearn)</li>
+              <li>Django Framwork Trainee (Infolabz Pvt. Ltd.)</li>
+            </ul>
+
+          </div>
 
         </div>
       </div>
